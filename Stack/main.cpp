b210259cdd6eab82509c12217fd59c9b127e8bc8
andregarcia0412 <andregarcia0412@gmail.com>
2026-04-24 23:52:20 -0300

    feat: add dynamic stack

---
+++ Stack/main.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *prev;
+
+    Node(int value)
+    {
+        this->value = value;
+        prev = nullptr;
+    }
+};
+
+class Stack
+{
+private:
+    Node *top;
+
+public:
+    Stack()
+    {
+        top = nullptr;
+    }
+
+    void push(int value)
+    {
+        Node *newNode = new Node(value);
+        newNode->prev = top;
+        top = newNode;
+    }
+
+    int pop()
+    {
+        if (isEmpty())
+        {
+            throw runtime_error("Stack Underflow!");
+        }
+
+        Node *removedNode = top;
+        int removedValue = removedNode->value;
+        top = top->prev;
+        delete removedNode;
+
+        return removedValue;
+    }
+
+    Node *peek()
+    {
+        return top;
+    }
+
+    void reverse()
+    {
+        Stack *newStack = new Stack();
+        while (!isEmpty())
+        {
+            newStack->push(pop());
+        }
+
+        top = newStack->peek();
+        delete newStack;
+    }
+
+    bool isEmpty()
+    {
+        return top == nullptr;
+    }
+
+    void print()
+    {
+        if (isEmpty())
+        {
+            cout << "[]" << endl;
+            return;
+        }
+
+        string str = "[";
+        Node *actual = top;
+        while (actual->prev != nullptr)
+        {
+            str += to_string(actual->value) + ",";
+            actual = actual->prev;
+        }
+
+        str += to_string(actual->value) + "]";
+        cout << str << endl;
+    }
+};
+
+int main()
+{
+    Stack stack = Stack();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+
+    stack.print();
+
+    stack.pop();
+    stack.reverse();
+
+    stack.print();
+    return 0;
+}
